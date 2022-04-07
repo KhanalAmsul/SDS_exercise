f948897ebb944ffee89091aa382092d775949c96
amsulk <58516414+KhanalAmsul@users.noreply.github.com>
2022-04-07 18:15:19 -0500

    Update r_code.R

--- r_code.R
+++ r_code.R
@@ -5,32 +5,41 @@ library(lubridate)
 
 
 date_to_download <- lubridate::today("US/Central") - 1
+
+
+date_to_download <- format(date_to_download, '%m-%d-%Y')
+
+base_URL <- paste0('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/',
+                   date_to_download,
+                   '.csv')
+
+if(httr::http_error(base_URL)){
+  message('There is no Data.')
+}else{
+  data <- readr::read_csv(base_URL) %>%
+    suppressWarnings() %>%
+    suppressMessages() %>%
+    subset(Country_Region == "US") %>%
+    subset(select = c('Province_State', 'Confirmed', 'Deaths')) %>%
+    group_by(Province_State)%>%
+    summarise_at(vars(Confirmed, Deaths), list(mean)) %>%
+    mutate(Confirmed = floor(Confirmed),
+           Deaths = floor(Deaths))%>%
+    dplyr::rename(State = Province_State,
+                  Avg_Confirmed = Confirmed,
+                  Avg_Deaths = Deaths) %>%
+    mutate(Date = date_to_download) %>%
+    subset(., select = c('Date', 'State', 'Avg_Confirmed', 'Avg_Deaths'))
   
-  
-  date_to_download <- format(date_to_download, '%m-%d-%Y')
-  
-  base_URL <- paste0('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/',
-                     date_to_download,
-                     '.csv')
-  
-  if(httr::http_error(base_URL)){
-    message('There is no Data.')
-  }else{
-    data <- readr::read_csv(base_URL) %>%
-      suppressWarnings() %>%
-      suppressMessages() %>%
-      subset(Country_Region == "US") %>%
-      subset(select = c('Province_State', 'Confirmed', 'Deaths')) %>%
-      group_by(Province_State)%>%
-      summarise_at(vars(Confirmed, Deaths), list(mean)) %>%
-      mutate(Confirmed = floor(Confirmed),
-             Deaths = floor(Deaths))%>%
-      dplyr::rename(State = Province_State,
-                    Avg_Confirmed = Confirmed,
-                    Avg_Deaths = Deaths) %>%
-      mutate(Date = date_to_download) %>%
-      subset(., select = c('Date', 'State', 'Avg_Confirmed', 'Avg_Deaths'))
-    
+  old_data <- read_csv('data/test_data.csv)
+    print('x1')
+
+    if(date_to_download %in% old_data$Date'){
+      print('data already there')
+    }else{
+    data <- rbind(old_data, data)
+    }
+
     write.csv(data, 'data/test_data.csv')
     
     
